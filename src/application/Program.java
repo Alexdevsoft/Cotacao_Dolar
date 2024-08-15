94d83505519a7fe49cf1b077225a78d1296faf45
Alexdevsoft <alexhavilla2022@gmail.com>
2024-08-15 10:23:49 -0300

    update

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,29 +3,27 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-import util.CurrencyConverter;
-
 public class Program {
-	
+
 	public static final double DOLLAR = 5.16;
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		double real = sc.nextDouble();
-		real = CurrencyConverter.valorDolar;
-		System.out.println("What is the dollar price? " + real);
-		double d = CurrencyConverter.converte();
-		d = sc.nextDouble();
-		
-		
-		System.out.printf("How many dollar will be bought? %.2f%n", d);
-		System.out.printf("Amount to be paid in reais = %.2f", real);
-		
-		
-		sc.close();
 
+		System.out.println("Quantos dólares serão comprados?");
+		double dollars = sc.nextDouble();
+
+		double reais = converte(dollars);
+
+		System.out.printf("Valor a ser pago em reais = %.2f%n", reais);
+
+		sc.close();
+	}
+
+	public static double converte(double dollars) {
+		return dollars * DOLLAR;
 	}
 
 }
